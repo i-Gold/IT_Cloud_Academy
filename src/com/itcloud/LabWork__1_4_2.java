08dd302348d53733612709e040fb040e8ff432d8
Richard <kamara.richard10@gmail.com>
2017-11-01 12:27:46 +0200

    Add some labs (to 1.4.1)

--- src/com/itcloud/LabWork__1_4_2.java
+++ src/com/itcloud/LabWork__1_4_2.java
@@ -1,4 +1,14 @@
 package com.itcloud;
 
 public class LabWork__1_4_2 {
+
+    public static void main(String[] args) {
+
+        int fitstNum = 23;
+        int secondNum = 32;
+
+        int excess = secondNum % fitstNum;
+
+        System.out.print(excess);
+    }
 }
--- src/com/itcloud/LabWork__1_4_3.java
+++ src/com/itcloud/LabWork__1_4_3.java
@@ -1,4 +1,14 @@
 package com.itcloud;
 
 public class LabWork__1_4_3 {
+
+    public static void main(String[] args) {
+
+        boolean bTrue = true;
+        boolean bFalse = false;
+
+        boolean resultXOR = bFalse ^ bTrue;
+
+        System.out.println(resultXOR);
+    }
 }
