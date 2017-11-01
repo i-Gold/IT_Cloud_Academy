8ef14c2e64ac6d0ff2fe008d3e8b20703a1bcc00
Richard <kamara.richard10@gmail.com>
2017-11-01 14:42:43 +0200

    Initial all labs for now

---
+++ src/com/itcloud/LabWork__1_4_8.java
@@ -0,0 +1,16 @@
+package com.itcloud;
+
+public class LabWork__1_4_8 {
+
+    public static void main(String[] args) {
+
+        int i = 100;
+        long l = (long)i;
+        float f = (float)l;
+
+        System.out.println("Without losing of data \n\n"
+                         + "Int : " + i + "\n"
+                         + "Long : " + l + "\n"
+                         + "Float : " + f);
+    }
+}
