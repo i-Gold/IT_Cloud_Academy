3c54efc3bd3953dda63a26f9db9f6dca5ae9bbba
Richard <kamara.richard10@gmail.com>
2017-11-03 16:54:57 +0200

    Init this

--- src/com/itcloud/Lab_Work___1_5_5.java
+++ src/com/itcloud/Lab_Work___1_5_5.java
@@ -1,4 +1,27 @@
 package com.itcloud;
 
 public class Lab_Work___1_5_5 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int averageValue = 0;
+        int counter;
+
+            for (int i = 1; i < 1000; i++){
+
+                counter = i;
+                sum += i;
+
+                if (sum > 100){
+
+                    break;
+                }
+
+                averageValue = sum / counter;
+            }
+
+        System.out.print("The Sum is: " + sum + "\n"
+                       + "The Avg. is: " + averageValue);
+    }
 }
--- src/com/itcloud/Lab_Work___1_5_7.java
+++ src/com/itcloud/Lab_Work___1_5_7.java
@@ -1,4 +1,33 @@
 package com.itcloud;
 
+import java.util.Scanner;
+
 public class Lab_Work___1_5_7 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        System.out.print("The perfect numbers are: ");
+
+        for (int i = 1; i <= n; i++)
+        {
+            int sum = 0;
+
+            for (int j = 1; j <= i/2; j++){
+
+                if(i % j == 0)
+                {
+                    sum += j;
+                }
+            }
+            if(sum == i){
+
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
