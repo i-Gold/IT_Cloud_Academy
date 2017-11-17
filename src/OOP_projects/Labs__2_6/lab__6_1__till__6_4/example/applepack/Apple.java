3137fe0c79a60d9bdc3b4b58ab12043d7c727cc8
Richard <kamara.richard10@gmail.com>
2017-11-17 13:23:50 +0200

    Done, but with some questions

---
+++ src/OOP_projects/Labs__2_6/lab__6_1__till__6_4/example/applepack/Apple.java
@@ -0,0 +1,14 @@
+package OOP_projects.Labs__2_6.lab__6_1__till__6_4.example.applepack;
+
+public class Apple {
+
+    private double weight;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
