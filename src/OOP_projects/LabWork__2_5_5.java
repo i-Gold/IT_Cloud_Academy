aca1819dc516cf75624b2d612e8466aca53d4dcf
Richard <kamara.richard10@gmail.com>
2017-11-17 12:05:19 +0200

    done

---
+++ src/OOP_projects/LabWork__2_5_5.java
@@ -0,0 +1,32 @@
+package OOP_projects;
+
+import java.util.Random;
+
+public class LabWork__2_5_5 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            InitTest init = new InitTest();
+            System.out.println(init.getId());
+        }
+    }
+}
+
+class InitTest {
+
+    private int id;
+    private static int nextId;
+
+    static {
+        Random random = new Random();
+        nextId = random.nextInt(1000 + 1);
+    }
+    {
+        nextId++;
+        id = nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
