eb13ae7b025b030271a99e3da5cdd6a9082aa19c
Richard <kamara.richard10@gmail.com>
2017-11-20 11:53:22 +0200

    chanded Main in 2-7-10

--- src/OOP_projects/Labs__2_7/LabWork__2_7_6_till_2_7_10/testshapes/Main.java
+++ src/OOP_projects/Labs__2_7/LabWork__2_7_6_till_2_7_10/testshapes/Main.java
@@ -3,55 +3,50 @@ package OOP_projects.Labs__2_7.LabWork__2_7_6_till_2_7_10.testshapes;
 public class Main {
     public static void main(String[] args) {
 
-        final int KINDS_OF_SHAPES= 3;
+        final int KINDS_OF_SHAPES = 9;
         double sumOfTotalArea = 0;
         double sumRectArea = 0;
         double sumTriangleArea = 0;
         double sumCircleArea = 0;
-        Shape[][] shapes = new Shape[KINDS_OF_SHAPES][];
+        Shape[] shapes = new Shape[KINDS_OF_SHAPES];
 
-        shapes[0] = new Shape[5];   // Why without this line is NullPointerException ?
-        shapes[0][0] = new Rectangle("RED", 11,22);
-        shapes[0][1] = new Rectangle("YELLOW", 10,21);
-        shapes[0][2] = new Rectangle("BROWN", 12,20);
-        shapes[0][3] = new Rectangle("GREY", 9,19);
-        shapes[0][4] = new Rectangle("PINK", 113,18);
+        shapes[0] = new Rectangle("RED", 11, 22);
+        shapes[1] = new Rectangle("YELLOW", 10, 21);
+        shapes[2] = new Rectangle("BROWN", 12, 20);
+        shapes[3] = new Rectangle("GREY", 9, 19);
+        shapes[4] = new Rectangle("PINK", 113, 18);
 
-        shapes[1] = new Shape[2];   // THIS LINE ?
-        shapes[1][0] = new Circle("GREEN", 10);
-        shapes[1][1] = new Circle("BLUE", 11);
+        shapes[5] = new Circle("GREEN", 10);
+        shapes[6] = new Circle("BLUE", 11);
 
-        shapes[2] = new Shape[2];   // AND THIS ?
-        shapes[2][0] = new Triangle("BLACK", 5,5,5);
-        shapes[2][1] = new Triangle("ORANGE", 7,7,7);
+        shapes[7] = new Triangle("BLACK", 5, 5, 5);
+        shapes[8] = new Triangle("ORANGE", 7, 7, 7);
 
-        for (Shape[] shapeType : shapes) {
-            for (Shape shape : shapeType)
+
+        for (Shape shape : shapes) {
             System.out.println("This is " + shape.toString() + "\n"
                     + "Shape area is: " + shape.calculateArea() + "\n");
         }
 
-        for (Shape[] shapeType : shapes) {
-            for (Shape shape : shapeType)
+        for (Shape shape : shapes) {
             sumOfTotalArea += shape.calculateArea();
         }
         System.out.println("The TOTAL AREA of all figures together: " + sumOfTotalArea + "\n");
 
-        for (Shape[] shapeType : shapes) {
-            for (Shape shape : shapeType){
-                if (shape instanceof Rectangle) {
-                    sumRectArea += shape.calculateArea();
-                }
-                if (shape instanceof Triangle) {
-                    sumTriangleArea += shape.calculateArea();
-                }
-                if (shape instanceof Circle) {
-                    sumCircleArea += shape.calculateArea();
-                }
+
+        for (Shape shape : shapes) {
+            if (shape instanceof Rectangle) {
+                sumRectArea += shape.calculateArea();
+            }
+            if (shape instanceof Triangle) {
+                sumTriangleArea += shape.calculateArea();
+            }
+            if (shape instanceof Circle) {
+                sumCircleArea += shape.calculateArea();
             }
         }
         System.out.println("Rectangles total area: " + sumRectArea + "\n"
-                         + "Triangles total area: " + sumTriangleArea + "\n"
-                         + "Circles total area: " + sumCircleArea + "\n");
+                + "Triangles total area: " + sumTriangleArea + "\n"
+                + "Circles total area: " + sumCircleArea + "\n");
     }
 }
